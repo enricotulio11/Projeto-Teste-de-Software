2009227659359244753852466eac98127e43ea2c
Arthur Silva <arthur.gbs.dev@gmail.com>
2026-05-26 17:12:45 -0300

    feat: adiciona todas as entidades TypeORM

---
+++ backend/src/entidades/configuracoes-sistema.entity.ts
@@ -0,0 +1,42 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('configuracoes_sistema')
+export class ConfiguracoesSistema {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ default: 'MedAgenda' })
+  nomeSistema: string;
+
+  @Column({ default: false })
+  modoManutencao: boolean;
+
+  @Column({ default: true })
+  permitirCadastros: boolean;
+
+  @Column({ default: 5 })
+  limiteDependentes: number;
+
+  @Column({ default: 30 })
+  tempoSessao: number;
+
+  @Column({ default: true })
+  habilitarLogsSeguranca: boolean;
+
+  @Column({ default: false })
+  exigirSenhaForte: boolean;
+
+  @Column({ default: false })
+  habilitarNotificacoesEmail: boolean;
+
+  @Column({ nullable: true })
+  emailNotificacao: string;
+
+  @UpdateDateColumn()
+  atualizadoEm: Date;
+}
